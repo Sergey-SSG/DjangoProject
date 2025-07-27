3f3e0025571965787287aef0c10f8a6b631592b8
Sergey <gaufss@gmail.com>
2025-07-27 11:07:19 +0500

    добавлено удаление

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     HomeView, ContactsView,
-    ProductDetailView, ProductCreateView, ProductUpdateView,
+    ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView,
 )
 
 app_name = "catalog"
@@ -12,6 +12,7 @@ urlpatterns = [
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("product/add/", ProductCreateView.as_view(), name="product_add"),
     path("product/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_edit"),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
 
 # from django.urls import path
