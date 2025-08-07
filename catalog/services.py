aac3411fe150107065f4be540c2af4d666cb7255
Sergey <gaufss@gmail.com>
2025-08-07 16:43:32 +0500

    29 Кеширование и бизнес-логика

--- catalog/services.py
+++ catalog/services.py
@@ -6,14 +6,7 @@ def get_products_by_category(category):
     key = f'products_category_{category.pk}'
     products = cache.get(key)
     if products is None:
-        products = Product.objects.filter(category=category, is_published=True)
+        products = Product.objects.filter(category=category, status='published')
         cache.set(key, products, 60 * 15)
     return products
 
-def get_all_products():
-    key = 'all_products'
-    products = cache.get(key)
-    if products is None:
-        products = Product.objects.filter(is_published=True)
-        cache.set(key, products, 60 * 10)
-    return products
